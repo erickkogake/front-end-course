812dba52eb38a591dcf2c3f40391d53049c6131f
Erick Kogake <erick.kogake@aigroup.local>
2022-07-22 09:59:08 -0300

    new fields

---
+++ NPM/new-package.js
@@ -0,0 +1,8 @@
+<html>
+    No node, podemos criar nosso proprio pacote com o - npm init e terá todas as confirmaçoes que teremos de fazer para criar.
+
+    Ou podemos usar o - npm init -y que já da 'yes' para tudo.
+
+    - npm i -D = Ele instala somente dependencias que podemos utilizar na área de desenvolvimento da aplicação.
+    Podemos fazer alterações no package.json, e dar um - npm update para atualizar essa modificação.
+</html>
